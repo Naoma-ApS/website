825e1ee4cf9f32b8bbc4efaac4c3371b0e321448
Jarl Pallesen <jpa@naoma.dk>
2024-04-02 16:26:27 +0200

    FIX: endpoints changed

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -35,7 +35,7 @@ export default function services() {
         <div className="my-8 grid gap-12 md:grid-cols-3">
           {/* Column 1 */}
           <div className="col-span-1">
-            <Link href="/services/railway">
+            <Link href="/services/Railway">
               <h2 className="flex items-center gap-2 text-lg font-normal hover:scale-105 hover:underline md:text-2xl">
                 <MdTrain className="text-xl md:text-4xl" /> Jernbane
               </h2>
@@ -43,13 +43,13 @@ export default function services() {
             <p className="text-md text-left ">
               <ul className="list-disc pl-8 text-base md:text-lg">
                 <Link
-                  href="/services/railway/starkstrom"
+                  href="/services/Railway/starkstrom"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Stærkstrøm</li>
                 </Link>
                 <Link
-                  href="/services/railway/sikkerhed"
+                  href="/services/Railway/sikkerhed"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Jernbanesikkerhed</li>
@@ -60,7 +60,7 @@ export default function services() {
 
           {/* Column 2 */}
           <div className="col-span-1">
-            <Link href="/services/trafficSystem">
+            <Link href="/services/TrafficSystem">
               <h2 className="mb-4 flex items-center gap-2 text-lg font-normal hover:scale-105 hover:underline md:text-2xl">
                 <FaRoad className="text-xl md:text-4xl" /> Vej system
               </h2>
@@ -68,15 +68,15 @@ export default function services() {
             <p className="text-md text-left ">
               <ul className="list-disc pl-8 text-base md:text-lg">
                 <Link
-                  href="/services/trafficSystem/ITS"
+                  href="/services/TrafficSystem/ITS"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">
-                    Intelligent Traffic System (ITS)
+                    Intelligent traffik System (ITS)
                   </li>
                 </Link>
                 <Link
-                  href="/services/trafficSystem/trafikAnalyse"
+                  href="/services/TrafficSystem/trafikAnalyse"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Trafik Analyse</li>
@@ -87,7 +87,7 @@ export default function services() {
 
           {/* Column 3 */}
           <div className="col-span-1">
-            <Link href="/services/management">
+            <Link href="/services/Management">
               <h2 className="mb-4 flex items-center gap-2 text-lg font-normal hover:scale-105 hover:underline md:text-2xl">
                 <LiaSearchDollarSolid className="text-xl md:text-4xl" />
                 Management
@@ -96,31 +96,31 @@ export default function services() {
             <p className="text-md text-left ">
               <ul className="list-disc pl-8 text-base md:text-lg">
                 <Link
-                  href="/services/management/byggeledelse"
+                  href="/services/Management/byggeledelse"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Byggeledelse</li>
                 </Link>
                 <Link
-                  href="/services/management/projektledelse"
+                  href="/services/Management/projektledelse"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Projektledelse</li>
                 </Link>
                 <Link
-                  href="/services/management/risikoledelse"
+                  href="/services/Management/risikoledelse"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Risikoledelse</li>
                 </Link>
                 <Link
-                  href="/services/management/strategiudvikling"
+                  href="/services/Management/strategiudvikling"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Strategiudvikling</li>
                 </Link>
                 <Link
-                  href="/services/management/undervisning"
+                  href="/services/Management/undervisning"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Undervisning</li>
@@ -132,7 +132,7 @@ export default function services() {
           {/* // ekspertise section */}
           {/* Column 4 */}
           <div className="col-span-1">
-            <Link href="/services/energy">
+            <Link href="/services/Energy">
               <h2 className="mb-4 flex items-center gap-2 text-lg font-normal hover:scale-105 hover:underline md:text-2xl">
                 <SlEnergy className="text-xl md:text-4xl" /> Energi
               </h2>
@@ -140,22 +140,22 @@ export default function services() {
             <p className="text-md text-left ">
               <ul className="list-disc pl-8 text-base md:text-lg">
                 <Link
-                  href="/services/energy/solarPower"
+                  href="/services/Energy/solarPower"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Solernergi</li>
                 </Link>
                 <Link
-                  href="/services/energy/windPower"
+                  href="/services/Energy/windPower"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Vindenergi</li>
                 </Link>
-                <Link href="/services/energy/elnet" className="hover:underline">
+                <Link href="/services/Energy/elnet" className="hover:underline">
                   <li className=" hover:scale-105">Elnet</li>
                 </Link>
                 <Link
-                  href="/services/energy/storage"
+                  href="/services/Energy/storage"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Lagring & BMS</li>
@@ -166,7 +166,7 @@ export default function services() {
 
           {/* Column 5 */}
           <div className="col-span-1 ">
-            <Link href="/services/clientConsultancy">
+            <Link href="/services/ClientConsultancy">
               <h2 className="mb-4 flex items-center gap-2 text-lg font-normal hover:scale-105 hover:underline md:text-2xl">
                 <BsBuildings className="text-xl md:text-4xl" />
                 Bygherrerådgivning
@@ -175,7 +175,7 @@ export default function services() {
             <p className="text-md text-left ">
               <ul className="list-disc pl-8 text-base md:text-lg">
                 <Link
-                  href="/services/clientConsultancy"
+                  href="/services/ClientConsultancy"
                   className="hover:underline"
                 >
                   <li className=" hover:scale-105">Opdateres...</li>
@@ -186,14 +186,14 @@ export default function services() {
 
           {/* Column 6 */}
           <div className="col-span-1">
-            <Link href="/services/automation">
+            <Link href="/services/Automation">
               <h2 className="mb-4 flex items-center gap-2 text-lg font-normal hover:scale-105 hover:underline md:text-2xl">
                 <LuCode2 className="text-xl md:text-4xl" /> Automation
               </h2>
             </Link>
             <p className="text-md text-left ">
               <ul className="list-disc pl-8 text-base md:text-lg">
-                <Link href="/services/automation" className="hover:underline">
+                <Link href="/services/Automation" className="hover:underline">
                   <li className=" hover:scale-105">Opdateres...</li>
                 </Link>
               </ul>
