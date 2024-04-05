162b0697830dcd3a83906b418706530fe58a4c30
Jarl Pallesen <jpa@naoma.dk>
2024-04-05 12:43:16 +0200

    FIX: change title size contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -52,9 +52,9 @@ export default function ContactPage() {
         />
         <div className="absolute inset-0 flex items-center justify-center">
           <header>
-            <h1 className="text-7xl font-bold text-white">Kontakt</h1>
+            <h1 className="text-center text-4xl md:text-7xl font-bold text-white">Kontakt</h1>
             <hr />
-            <div className="text-center text-2xl text-white">
+            <div className="text-center text-lg md:text-2xl text-white">
               Søg nye muligheder
             </div>
           </header>
