dd88a6bb797291d03e20b5056fa1d8e5b5a82846
Jarl Pallesen <jpa@naoma.dk>
2024-04-09 13:54:37 +0200

    CHORE:Added Naoma to title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { type Metadata } from "next";
 import HomePage from "./home/page";
 
 export const metadata: Metadata = {
-  title: "Rådgivning i harmoni",
+  title: "NAOMA: Rådgivning i harmoni",
 
   description:
     "Velkommen til vores ingeniørrådgivningsfirma, hvor erfaring møder innovation. Vores team af højt kvalificerede ingeniører bringer årtiers ekspertise til hvert projekt, sikrer bæredygtige og effektive løsninger på tværs af en bred vifte af industrier. Vi forpligter os til at skabe værdi og fremme fremskridt gennem vores dedikerede rådgivningstjenester. Lad os sammen bygge fremtiden.",
